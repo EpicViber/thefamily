13fd9444f2205ee26d8250c9e765a03a4a21e41c
EpicVibe <58629230+EpicViber@users.noreply.github.com>
2022-12-29 17:42:47 +0100

    d
    d

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
 export default function Header({ title }) {
   return <h1 className="title">{title}</h1>
 }
+<h2>Click here to see "<a target="_blank" href="https://thefamilymembers.netlify.app">The Higher Ups</a>".</h2>
