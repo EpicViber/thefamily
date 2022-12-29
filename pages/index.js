d65406d8c18ec7caeb3e7e015f5f39c7da2434d8
EpicVibe <58629230+EpicViber@users.noreply.github.com>
2022-12-29 17:52:05 +0100

    d
    d

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,8 @@ export default function Home() {
         <Header title="THE FAMILY" />
         <p className="description">
           <code>A group owned by a few idiots on discord</code>
-          <h2>Click here to see "<a target="_blank" href="https://thefamilymembers.netlify.app">The Higher Ups</a>".</h2>
         </p>
+        <p>Click here to see "<a target="_blank" href="https://thefamilymembers.netlify.app">The Higher Ups</a>".</p>
         <img src="https://cdn.discordapp.com/attachments/565187025058136075/1058000579101732934/DALLE_2022-10-03_18.55.19_-_only_in_ohio.png" alt="group icon" width="450" height="450"></img>
       </main>
       <Footer />
